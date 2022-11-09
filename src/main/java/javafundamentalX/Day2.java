35a729955a1a8d84a23a2fe7e0aab9dc27793ec8
Andara <Andara@Leo>
2022-11-09 09:20:31 +0700

    Coba

--- src/main/java/javafundamentalX/Day2.java
+++ src/main/java/javafundamentalX/Day2.java
@@ -2,6 +2,15 @@ package javafundamentalX;
 
 import java.util.*; // menambahkan fungsi untuk random 
 
+/*
+@Author Andara a.k.a. Sandhy
+Junior Programmer
+Created with Eclipse version 2022-09(4.25.0)
+Created on 09-11-2022 09:09:09
+Last Modified on 09-11-2022 09:09:09
+Version 1.0
+*/
+
 public class Day2 {
 	public static void main (String args[]) {
 		String strA = "Tb@14XzM";
